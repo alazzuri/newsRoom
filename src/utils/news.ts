c8cbcf795e0cc732c08101cb05ac09fc1993ff06
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-08-05 20:56:18 -0300

    Adding category number creator

--- src/utils/news.ts
+++ src/utils/news.ts
@@ -11,3 +11,23 @@ export const newsNormalizer = (NewsArray: Array<NewsResponse>) =>
         url: news.url,
       }
   );
+
+export const categoryNumber = (category: string) => {
+  switch (category.toLowerCase()) {
+    case "política":
+      return 1;
+    case "internacionales":
+      return 2;
+    case "tecnología":
+      return 3;
+    case "espectáculos":
+      return 4;
+    case "deportes":
+      return 5;
+    case "diseño":
+      return 6;
+
+    default:
+      return null;
+  }
+};
